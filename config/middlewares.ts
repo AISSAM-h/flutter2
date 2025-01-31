a2555d86c6aa3750b9c36405c21140de23741963
aisssam <aissaminfo2015@gmail.com>
2025-01-31 08:05:21 +0100

    first commit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,29 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // Allow all origins (or specify your Flutter app's origin, e.g., 'http://localhost')
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], // Allowed HTTP methods
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'], // Allowed headers
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
